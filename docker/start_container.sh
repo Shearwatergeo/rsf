17151fdb18741d3d70d2e41d16141055b8261f37
Marcel Nauta <mnauta@shearwatergeo.com>
2020-07-02 22:17:35 +0100

    Fix manpage in docker build environment

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -5,7 +5,11 @@ echo ""
 echo "To build the project, run:"
 echo "    scl enable devtoolset-7 bash"
 echo "    cd /nfs/rsf"
-echo "    . /etc/profile.d/*.sh"
-echo "    ./configure --prefix=/opt/madagascar"
+echo "    #. /etc/profile.d/*.sh # Does not currently do anything"
+echo '    export PATH=/opt/anaconda/bin:$PATH'
+echo "    ./configure --prefix=/nfs/rsf/build_docker"
+echo "    make"
+echo "    make install"
+echo "    source /nfs/rsf/build_docker/share/madagascar/etc/env.sh
 docker run --user $(id -u):$(id -g) -v $(pwd):/nfs/rsf -ti madagascar
 #docker run -ti madagascar
